b6022082848c004006c0374c57f6ce2ee9bbfdf3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-07 02:39:28 -0400

    [tx-robot] updated from transifex

--- l10n/gl.php
+++ l10n/gl.php
@@ -3,9 +3,14 @@ $TRANSLATIONS = array(
 "Could not load template" => "Non foi posíbel cargar o modelo",
 "Saved" => "Gardado",
 "Reset" => "Restabelecer",
+"Sharing email (http)" => "Compartir por correo (http)",
+"Sharing email" => "Compartir por correo",
+"Lost password mail" => "Correo de contrasinal perdido",
+"Activity notification mail" => "Correo de notificación de actividade",
 "Mail templates" => "Modelos de correo",
 "Theme" => "Tema",
 "Template" => "Modelo",
+"Please choose a template" => "Escolle un modelo",
 "Save" => "Gardar"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/pt_PT.php
+++ l10n/pt_PT.php
@@ -2,6 +2,10 @@
 $TRANSLATIONS = array(
 "Saved" => "Guardado",
 "Reset" => "Repor",
+"Mail templates" => "Modelo de email",
+"Theme" => "Tema",
+"Template" => "Template",
+"Please choose a template" => "Por favor escolha um modelo",
 "Save" => "Guardar"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
