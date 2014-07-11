98e8713c933256f46bdbf88a2c7849b0063af9f0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-11 04:12:04 -0400

    [tx-robot] updated from transifex

--- l10n/et_EE.php
+++ l10n/et_EE.php
@@ -1,7 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Could not load template" => "Ei suutnud malli laadida",
 "Saved" => "Salvestatud",
 "Reset" => "Algseaded",
+"Sharing email (http)" => "Jagamise e-mail (http)",
+"Sharing email" => "Jagamise e-mail",
+"Lost password mail" => "Unustatud parooli kiri",
+"Activity notification mail" => "Tegevuste teavituse kiri",
+"Mail templates" => "Kirja mallid",
+"Theme" => "Teema",
+"Template" => "Mall",
+"Please choose a template" => "Palun vali mall",
 "Save" => "Salvesta"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/sl.php
+++ l10n/sl.php
@@ -1,11 +1,16 @@
 <?php
 $TRANSLATIONS = array(
-"Could not load template" => "Predloge ni bilo mogoče naložiti",
+"Could not load template" => "Ni mogoče naložiti predloge",
 "Saved" => "Shranjeno",
 "Reset" => "Ponastavi",
-"Mail templates" => "Predloga elektronske pošte",
+"Sharing email (http)" => "Elektronsko sporočilo za odobritev souporabe (http)",
+"Sharing email" => "Elektronsko sporočilo za odobritev souporabe",
+"Lost password mail" => "Elektronsko sporočilo za pozabljeno geslo",
+"Activity notification mail" => "Elektronsko sporočilo za obvestolo o dejavnostih",
+"Mail templates" => "Predloge elektronskih sporočil",
 "Theme" => "Tema",
 "Template" => "Predloga",
+"Please choose a template" => "Izberite predlogo",
 "Save" => "Shrani"
 );
 $PLURAL_FORMS = "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);";
