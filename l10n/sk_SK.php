b9b2bdebc679298f60fd34792fe115a8a4858d7c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-15 02:39:29 -0400

    [tx-robot] updated from transifex

--- l10n/sk_SK.php
+++ l10n/sk_SK.php
@@ -3,9 +3,14 @@ $TRANSLATIONS = array(
 "Could not load template" => "Šablónu nemožno nahrať",
 "Saved" => "Uložené",
 "Reset" => "Resetovať",
+"Sharing email (http)" => "Zdieľať email (http)",
+"Sharing email" => "Zdieľať email",
+"Lost password mail" => "Email so zabudnutým heslom",
+"Activity notification mail" => "Oznámenia o aktivite emailom",
 "Mail templates" => "Emailové šablóny",
 "Theme" => "Téma",
 "Template" => "Šablóna",
+"Please choose a template" => "Zvoľte si šablónu",
 "Save" => "Uložiť"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";
