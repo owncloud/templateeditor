5f5f7241f8e18849b4ec5502b387bdf94ba9d787
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-27 02:39:27 -0400

    [tx-robot] updated from transifex

--- l10n/eu.php
+++ l10n/eu.php
@@ -1,7 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Could not load template" => "Ezin da txantiloia kargatu",
 "Saved" => "Gordeta",
 "Reset" => "Berrezarri",
+"Sharing email (http)" => "E-posta konpartitzen (http)",
+"Sharing email" => "E-posta konpartitzen",
+"Lost password mail" => "Postaren pasahitza galduta",
+"Activity notification mail" => "Jarduera jakinarazpen mezua",
+"Mail templates" => "Posta txantiloia",
+"Theme" => "Gaia",
+"Template" => "Txantiloia",
+"Please choose a template" => "Aukeratu txantiloi bat",
 "Save" => "Gorde"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
