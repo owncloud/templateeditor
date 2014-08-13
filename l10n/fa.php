8c85af9b8e2103bffc4af822fa385542297e5014
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-13 02:39:12 -0400

    [tx-robot] updated from transifex

--- l10n/fa.php
+++ l10n/fa.php
@@ -1,6 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Could not load template" => "امکان بارگذاری قالب وجود ندارد",
+"Saved" => "ذخیره شد",
 "Reset" => "تنظیم مجدد",
+"Sharing email (http)" => "اشتراک گذاری ایمیل (HTTP)",
+"Sharing email" => "اشتراک ایمیل",
+"Lost password mail" => "ایمیل فراموش کردن رمز عبور",
+"Activity notification mail" => "ایمیل هشدار فعالیت",
+"Mail templates" => "قالب های ایمیل",
+"Theme" => "تم",
+"Template" => "قالب",
+"Please choose a template" => "لطفا یک قالب انتخاب کنید",
 "Save" => "ذخیره"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
