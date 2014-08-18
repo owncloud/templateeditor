1f176f69c21ccd0df20b244e40e14f8d82b23484
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-18 02:39:12 -0400

    [tx-robot] updated from transifex

--- l10n/bn_IN.php
+++ l10n/bn_IN.php
@@ -1,5 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Could not load template" => "টেমপ্লেট লোড করা যায়নি",
+"Saved" => "সংরক্ষিত",
+"Reset" => "রিসেট করুন",
+"Sharing email (http)" => "ইমেইল শেয়ার করা(http)",
+"Sharing email" => "ইমেইল শেয়ার করা",
+"Lost password mail" => "হারানো পাসওয়ার্ডের জন্যে মেইল",
+"Activity notification mail" => "কার্যকলাপের বিজ্ঞপ্তি মেইল",
+"Mail templates" => "মেইল টেমপ্লেত",
+"Theme" => "থিম",
+"Template" => "টেম্পলেট ",
+"Please choose a template" => "একটি টেম্পলেট বেছে নিন",
 "Save" => "সেভ"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
