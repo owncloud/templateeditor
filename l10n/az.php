8db66cd65058d37b8855f5f84d55fee646f4e3bf
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-19 02:39:12 -0400

    [tx-robot] updated from transifex

---
+++ l10n/az.php
@@ -0,0 +1,14 @@
+<?php
+$TRANSLATIONS = array(
+"Could not load template" => "Şablonu yükləmək mümkün olmadı",
+"Saved" => "Saxlanıldı",
+"Reset" => "Sıfırla",
+"Sharing email (http)" => "Məktub yayımlanması(http)",
+"Sharing email" => "Məktubun yayımlanması",
+"Mail templates" => "Məktub şablonları",
+"Theme" => "Mövzu",
+"Template" => "Şablon",
+"Please choose a template" => "Xahiş olunur şablonu seçəsiniz",
+"Save" => "Saxlamaq"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
