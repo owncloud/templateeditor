5036682f39d61cf560a58f3c4a723d17432b5021
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-29 02:39:14 -0400

    [tx-robot] updated from transifex

--- l10n/bn_BD.php
+++ l10n/bn_BD.php
@@ -1,5 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Could not load template" => "টেমপ্লেট লোড করা গেলনা",
+"Saved" => "সংরক্ষণ করা হলো",
+"Reset" => "পূণঃনির্ধানণ",
+"Sharing email (http)" => "ইমেইল ভাগাভাগি করা হচ্ছে (http)",
+"Sharing email" => "ইমেইল ভাগাভাগি করা হচ্ছে",
+"Lost password mail" => "কুটশব্দের মেইল হরিয়ে গেছে",
+"Activity notification mail" => "কার্যক্রম বার্তার মেইল",
+"Mail templates" => "মেইল টেমপ্লেট",
+"Theme" => "থিম",
+"Template" => "টেমপ্লেট",
+"Please choose a template" => "দয়া করে একটি টেমপ্লেট নির্বাচন করুন",
 "Save" => "সংরক্ষণ"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
