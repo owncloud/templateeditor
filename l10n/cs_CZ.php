18396922fd1f5e6463e2742656f453717876e706
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-31 02:39:13 -0400

    [tx-robot] updated from transifex

--- l10n/cs_CZ.php
+++ l10n/cs_CZ.php
@@ -6,11 +6,11 @@ $TRANSLATIONS = array(
 "Sharing email (http)" => "Sdílet e-mail (http)",
 "Sharing email" => "Sdílet e-mail",
 "Lost password mail" => "E-mail se zapomenutým heslem",
-"Activity notification mail" => "Aktivovat emailová upozornění",
-"Mail templates" => "E-mailové šablony",
+"Activity notification mail" => "E-mailová upozornění na aktivitu",
+"Mail templates" => "Šablony e-mailů",
 "Theme" => "Vzhled",
 "Template" => "Šablona",
-"Please choose a template" => "Zvol prosím šablonu",
+"Please choose a template" => "Zvolte prosím šablonu",
 "Save" => "Uložit"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";
