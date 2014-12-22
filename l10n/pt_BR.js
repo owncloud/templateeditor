12677d7435850df026baf7d0bfbd428cececd9db
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-22 02:39:11 -0500

    [tx-robot] updated from transifex

--- l10n/pt_BR.js
+++ l10n/pt_BR.js
@@ -7,6 +7,8 @@ OC.L10N.register(
     "Sharing email (HTML)" : "Compartilhando email (HTML)",
     "Sharing email (plain text fallback)" : "Compartilhando email (recurso texto simples)",
     "Lost password mail" : "Perdeu senha do email",
+    "New user email (HTML)" : "Novo e-mail do usuário (HTML)",
+    "New user email (plain text fallback)" : "Novo e-mail do usuário (fallback texto simples)",
     "Activity notification mail" : "Email de notificação de Atividade",
     "Mail Templates" : "Modelos de Email",
     "Theme" : "Tema",
