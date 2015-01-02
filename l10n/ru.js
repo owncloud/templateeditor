a0aecbd5bdcf62b6b55ec53bc368831663264d33
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-02 02:39:09 -0500

    [tx-robot] updated from transifex

--- l10n/ru.js
+++ l10n/ru.js
@@ -13,7 +13,7 @@ OC.L10N.register(
     "Mail Templates" : "Шаблоны электронных писем",
     "Theme" : "Тема",
     "Template" : "Шаблон",
-    "Please choose a template" : "Пожалуйста, выберите шаблон",
+    "Please choose a template" : "Выберите шаблон",
     "Save" : "Сохранить"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
--- l10n/ru.php
+++ l10n/ru.php
@@ -12,7 +12,7 @@ $TRANSLATIONS = array(
 "Mail Templates" => "Шаблоны электронных писем",
 "Theme" => "Тема",
 "Template" => "Шаблон",
-"Please choose a template" => "Пожалуйста, выберите шаблон",
+"Please choose a template" => "Выберите шаблон",
 "Save" => "Сохранить"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
