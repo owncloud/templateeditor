449f8f02c6a46f01368ad852f6adf9ea684a1042
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-06 02:39:12 -0500

    [tx-robot] updated from transifex

--- l10n/eu.js
+++ l10n/eu.js
@@ -7,6 +7,7 @@ OC.L10N.register(
     "Sharing email (HTML)" : "Partekatzeko eposta (HTML)",
     "Lost password mail" : "Postaren pasahitza galduta",
     "Activity notification mail" : "Jarduera jakinarazpen mezua",
+    "Mail Templates" : "Posta Txantiloiak",
     "Theme" : "Gaia",
     "Template" : "Txantiloia",
     "Please choose a template" : "Aukeratu txantiloi bat",
