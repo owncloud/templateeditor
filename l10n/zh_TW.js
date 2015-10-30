79f866116505e63fb7fb01bc49131c75daad2c37
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-30 02:39:26 -0400

    [tx-robot] updated from transifex

---
+++ l10n/zh_TW.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "templateeditor",
+    {
+    "Could not load template" : "無法載入樣板",
+    "Saved" : "已儲存",
+    "Reset" : "重設",
+    "Sharing email (HTML)" : "分享電子郵件(HTML)",
+    "Sharing email (plain text fallback)" : "分享電子郵件(純文本回傳)",
+    "Lost password mail" : "遺失密碼信",
+    "New user email (HTML)" : "新使用者的電子郵件(HTML)",
+    "New user email (plain text fallback)" : "新使用者的電子郵件(純文本回傳)",
+    "Activity notification mail" : "活動通知信件",
+    "Mail Templates" : "信件樣板",
+    "Theme" : "佈景主題",
+    "Template" : "樣板格式",
+    "Please choose a template" : "請選擇一個樣板",
+    "Save" : "儲存"
+},
+"nplurals=1; plural=0;");
