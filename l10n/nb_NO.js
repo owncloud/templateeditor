3f9a0eb8f8b6869400bd77c3023908616f2912d3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-04 02:39:21 -0500

    [tx-robot] updated from transifex

--- l10n/nb_NO.js
+++ l10n/nb_NO.js
@@ -5,6 +5,8 @@ OC.L10N.register(
     "Saved" : "Lagret",
     "Reset" : "Tilbakestill",
     "An error occurred" : "En feil har oppstått",
+    "Sharing email - public link shares (HTML)" : "Delings-epost - offentlige lenke-delinger (HTML)",
+    "Sharing email - public link shares (plain text fallback)" : "Delings-epost - offentlige lenke-delinger (ren tekst)",
     "Sharing email (HTML)" : "Delings-epost (HTML)",
     "Sharing email (plain text fallback)" : "Delings-epost (ren tekst)",
     "Lost password mail" : "E-post om mistet passord",
