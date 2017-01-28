a153815d048d77067477a61f626b0392af15cf8f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-01-28 02:39:15 +0100

    [tx-robot] updated from transifex

---
+++ l10n/es_MX.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "templateeditor",
+    {
+    "Could not load template" : "No se puede cargar la plantilla",
+    "Saved" : "Guardado",
+    "Reset" : "Reiniciar",
+    "An error occurred" : "Ocurrió un error",
+    "Sharing email - public link shares (HTML)" : "Compartir por correo electrónico - compartir enlace público (HTML)",
+    "Sharing email - public link shares (plain text fallback)" : "Compartir por correo electrónico - compartir enlace público (Texto simple)",
+    "Sharing email (HTML)" : "Compartir por correo electrónico (HTML)",
+    "Sharing email (plain text fallback)" : "Compartir por correo electrónico (Texto simple)",
+    "Lost password mail" : "Correo electrónico - contraseña perdida",
+    "New user email (HTML)" : "Correo electrónico - nuevo usuario (HTML)",
+    "New user email (plain text fallback)" : "Correo electrónico - nuevo usuario (Texto simple)",
+    "Activity notification mail" : "Correo electrónico de notificación de actividad",
+    "Mail Templates" : "Plantillas de correo",
+    "Theme" : "Temas",
+    "Template" : "Plantilla",
+    "Please choose a template" : "Por favor elije una plantilla",
+    "Save" : "Guardar"
+},
+"nplurals=2; plural=(n != 1);");
