acc7753f897611a81a9ae2e4d26404e657354ac0
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-05 02:06:20 +0000

    [tx-robot] updated from transifex

---
+++ l10n/pt_PT.php
@@ -0,0 +1,21 @@
+<?php
+$TRANSLATIONS = array(
+"Could not load template" => "Não foi possível carregar o modelo",
+"Saved" => "Guardado",
+"Reset" => "Repor",
+"An error occurred" => "Ocorreu um erro",
+"Sharing email - public link shares (HTML)" => "Correio eletrónico da partilha - partilhas de ligação pública (HTML)",
+"Sharing email - public link shares (plain text fallback)" => "Correio eletrónico da partilha - partilhas de ligação pública (texto simples)",
+"Sharing email (HTML)" => "Partilhar correio eletrónico (HTML)",
+"Sharing email (plain text fallback)" => "Partilhar mensagem (texto sem formatação)",
+"Lost password mail" => "Perdi a palavra passe do correio",
+"New user email (HTML)" => "Novo e-mail de utilizador (HTML)",
+"New user email (plain text fallback)" => "Novo email de utilizador (texto simples)",
+"Activity notification mail" => "Email de notificação de actividade",
+"Mail Templates" => "Modelos de Correio",
+"Theme" => "Tema",
+"Template" => "Modelo",
+"Please choose a template" => "Por favor, escolha um modelo",
+"Save" => "Guardar"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/uk.php
@@ -0,0 +1,21 @@
+<?php
+$TRANSLATIONS = array(
+"Could not load template" => "Неможливо завантажити шаблон",
+"Saved" => "Збереженно",
+"Reset" => "Перевстановити",
+"An error occurred" => "Виникла помилка",
+"Sharing email - public link shares (HTML)" => "Лист про відкриття спільного доступу - публічні посилання для доступу (HTML)",
+"Sharing email - public link shares (plain text fallback)" => "Лист про відкриття спільного доступу - публічні посилання для доступу (тільки текст)",
+"Sharing email (HTML)" => "Лист про відкриття спільного доступу (HTML)",
+"Sharing email (plain text fallback)" => "Лист про відкриття спільного доступу (тількі текст)",
+"Lost password mail" => "Лист відновлення пароля",
+"New user email (HTML)" => "Лист про нового користувача (HTML)",
+"New user email (plain text fallback)" => "Лист про нового користувача (тільки текст)",
+"Activity notification mail" => "Лист повідомлення про активність",
+"Mail Templates" => "Шаблони Листів",
+"Theme" => "Тема",
+"Template" => "Шаблон",
+"Please choose a template" => "Будь ласка, оберіть шаблон",
+"Save" => "Зберегти"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
