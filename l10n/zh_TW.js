877fab5ac77e88fb91d6ae88236f1593c77b2249
Thomas Müller <thomas.mueller@tmit.eu>
2020-09-23 00:07:33 +0000

    [tx] updated from transifex

--- l10n/zh_TW.js
+++ l10n/zh_TW.js
@@ -12,11 +12,18 @@ OC.L10N.register(
     "Lost password mail" : "遺失密碼信",
     "New user email (HTML)" : "新使用者的電子郵件(HTML)",
     "New user email (plain text fallback)" : "新使用者的電子郵件(純文本回傳)",
-    "Activity notification mail" : "活動通知信件",
-    "Mail Templates" : "信件樣板",
+    "Common email footer (HTML)" : "常用電子郵件結尾 (HTML)",
+    "Common email footer (plain text)" : "常用電子郵件結尾 (純文字)",
+    "Activity notification mail (HTML)" : "活動通知郵件 (HTML)",
+    "Activity notification mail (plain text)" : "活動通知郵件 (純文字)",
+    "Notifications app mail (HTML)" : "通知軟體郵件 (HTML)",
+    "Notifications app mail (plain text)" : "通知軟體郵件 (純文字)",
+    "Mail Templates" : "郵件範本",
     "Theme" : "佈景主題",
-    "Template" : "樣板格式",
-    "Please choose a template" : "請選擇一個樣板",
-    "Save" : "儲存"
+    "Template" : "範本格式",
+    "Please choose a template" : "請選擇一個範本",
+    "Save" : "儲存",
+    "You need to activate own theme to edit mail templates." : "您需要啟動自己的主題來編輯郵件範本。",
+    "How to <a target=\"_blank\" rel=\"noreferrer\" href=\"%s\">create a theme</a>." : "需要<a target=\"_blank\" rel=\"noreferrer\" href=\"%s\">建立一個主題</a>。"
 },
 "nplurals=1; plural=0;");
